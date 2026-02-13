093214014be7a25c6a459a06fea64cafa027222f
Ankit Gupta <ag5756042@gmail.com>
2026-02-13 23:58:10 +0530

    feat: add category management with images and implement CategoryCard component in UserDashboard

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,10 +1,22 @@
 
+import { categories } from "../category.js"
+import CategoryCard from "./CategoryCard"
 import Nav from "./Nav"
 function UserDashboard() {
   return (
-    <>
-    <Nav />
-    </>
+    <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-[#fff9f6] overflow-y-auto">
+      <Nav />
+      <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]">
+        <h1 className="text-gray-800 text-2xl sm:text-3xl">Inspiration for your first order</h1>
+        <div className="w-full">
+          <div className="w-full flex overflow-x-auto gap-4 pb-2">
+            {categories.map((cate, index) => (
+              <CategoryCard data={cate} key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
