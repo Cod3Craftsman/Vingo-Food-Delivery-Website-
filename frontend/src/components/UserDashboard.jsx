fb5d01e9f9190b887a657cd9692e9565ef229472
Ankit Gupta <ag5756042@gmail.com>
2026-02-18 16:38:42 +0530

    feat: implement horizontal scrolling for category cards with navigation buttons in UserDashboard

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,19 +1,67 @@
 
 import { categories } from "../category.js"
 import CategoryCard from "./CategoryCard"
+import { FaCircleChevronLeft } from "react-icons/fa6"
+import { FaCircleChevronRight } from "react-icons/fa6"
 import Nav from "./Nav"
+import { useEffect, useRef, useState } from "react"
 function UserDashboard() {
+  const cateScrollRef = useRef()
+  const [showLeftCateButton, setShowLeftCateButton] = useState(false)
+  const [showRightCateButton, setShowRightCateButton] = useState(true)
+
+
+  const updateButton = (ref, setLeftButton, setRightButton) => {
+    const element = ref.current
+    if (element) {
+      setLeftButton(element.scrollLeft > 0)
+      setRightButton(element.scrollLeft + element.clientWidth < element.scrollWidth)
+    }
+  }
+  const srollHandler = (ref, direction) => {
+    if (ref.current) {
+      ref.current.scrollBy({
+        left: direction == "left" ? -200 : 200,
+        behavior: "smooth"
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (cateScrollRef.current) {
+      cateScrollRef.current.addEventListener('scroll', () => {
+        updateButton(cateScrollRef, setShowLeftCateButton, setShowRightCateButton)
+      })
+    }
+    return () => {
+      element.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
     <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-[#fff9f6] overflow-y-auto">
       <Nav />
       <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]">
         <h1 className="text-gray-800 text-2xl sm:text-3xl">Inspiration for your first order</h1>
-        <div className="w-full">
-          <div className="w-full flex overflow-x-auto gap-4 pb-2">
+        <div className="w-full relative">
+          {showLeftCateButton &&
+            <button className="absolute left-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 cursor-pointer" onClick={() => srollHandler(cateScrollRef, "left")}>
+              <FaCircleChevronLeft />
+            </button>
+          }
+
+          <div className="w-full flex overflow-x-auto gap-4 pb-2" ref={cateScrollRef}>
             {categories.map((cate, index) => (
               <CategoryCard data={cate} key={index} />
             ))}
           </div>
+
+          {showRightCateButton &&
+            <button className="absolute right-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 cursor-pointer" onClick={() => srollHandler(cateScrollRef, "right")}>
+              <FaCircleChevronRight />
+            </button>
+          }
+
         </div>
       </div>
     </div>
